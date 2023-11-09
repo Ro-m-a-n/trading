27244a8c9c200f5c7f5d5cd0a7376977d643e25e
roman liutyk <ggyrda18@gmail.com>
2023-11-09 14:08:30 +0200

    updated form logic

--- src/components/contactUs/form/form.jsx
+++ src/components/contactUs/form/form.jsx
@@ -1,19 +1,17 @@
-// export const Form = () => {};
-
 import { useFormik } from "formik";
 import { validate } from "./validation";
-import Input from "./Input";
 
 export const Form = () => {
   const formik = useFormik({
+    enableReinitialize: true,
     initialValues: {
       name: "",
       email: "",
       message: "",
     },
     validate: (values) => validate(values, formik),
-    validateOnChange: true,
-    // validateOnBlur: true,
+    validateOnChange: false,
+    validateOnBlur: true,
 
     onSubmit: (values) => {
       console.log("submit data", values.name, values.email, values.message);
@@ -21,46 +19,48 @@ export const Form = () => {
     },
   });
 
+  const { handleSubmit, errors, touched, getFieldProps, values, isValid } =
+    formik;
   return (
-    <form onSubmit={formik.handleSubmit} className="form_wrap" id="login-form">
-      <Input
+    <form onSubmit={handleSubmit} className="form_wrap" id="login-form">
+      <input
         name="name"
         placeholder={"Name"}
-        error={formik.errors.name}
-        {...formik.getFieldProps("name")}
+        style={{
+          borderColor: touched.name && (errors.name ? "red" : "green"),
+        }}
+        {...getFieldProps("name")}
       />
-
-      <Input
+      {errors.name && touched.name && (
+        <div className="text_error">{errors.name}</div>
+      )}
+      <input
         name="email"
         placeholder={"Email"}
         type="email"
-        error={formik.errors.email}
-        {...formik.getFieldProps("email")}
+        style={{
+          borderColor: touched.email && (errors.email ? "red" : "green"),
+        }}
+        {...getFieldProps("email")}
       />
-      <Input
+      {errors.email && touched.email && (
+        <div className="text_error">{errors.email}</div>
+      )}
+      <textarea
         name="message"
         placeholder={"Message"}
-        error={formik.errors.message}
-        {...formik.getFieldProps("message")}
+        style={{
+          borderColor: touched.message && (errors.message ? "red" : "green"),
+        }}
+        {...getFieldProps("message")}
       />
-
-      {/* <textarea
-        name="message"
-        placeholder={"Message"}
-        error={formik.errors.message}
-        {...formik.getFieldProps("message")}
-      /> */}
+      {errors.message && touched.message && (
+        <div className="text_error">{errors.message}</div>
+      )}
 
       <button
         type="submit"
-        disabled={
-          !(
-            formik.isValid &&
-            formik.values.name &&
-            formik.values.email &&
-            formik.values.message
-          )
-        }
+        disabled={!(isValid && values.name && values.email && values.message)}
       >
         SEND
       </button>
--- src/components/contactUs/form/validation.js
+++ src/components/contactUs/form/validation.js
@@ -1,20 +1,24 @@
 import { emailPattern } from "./pattern";
 
-export const validate = (values, formik) => {
+export const validate = (values) => {
   const errors = {};
-  if (formik.touched.name && !values.name) {
-    errors.name = "Field is required";
-  } else if (
-    formik.touched.name &&
-    (values.name.length < 2 || values.name.length > 60)
-  ) {
+  if (!values.name) {
+    console.log("values", values.name);
+    errors.name = "field is required";
+  } else if (values.name.length < 2 || values.name.length > 60) {
     errors.name = "Must be from 2 to 60 characters";
   }
 
-  if (formik.touched.email && !values.email) {
+  if (!values.email) {
     errors.email = "Field is required";
-  } else if (formik.touched.email && !emailPattern.test(values.email)) {
+  } else if (!emailPattern.test(values.email)) {
     errors.email = "Invalid email address";
+  }
+
+  if (!values.message) {
+    errors.message = "Field is required";
+  } else if (values.message.length < 2 || values.message.length > 100) {
+    errors.message = "Must be from 2 to 100 characters";
   }
   return errors;
 };
